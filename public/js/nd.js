3e03ce0cab1a0080caecc6fe789271fdcc30ce17
BA4 <arian@brandambassadors.nl>
2012-12-21 17:46:21 +0100

    bug uit google maps

--- public/js/nd.js
+++ public/js/nd.js
@@ -43,8 +43,7 @@ $(document).bind('pagechange' , function(){
 
 		GMap.init();
 
-
-		$( '.header-knop-zoek' ).on( 'click' , function(){ GMap.initGeo(); } )
+		$( '.header-knop-zoek' ).on( 'click' , function(){ GMap.init(); } )
 	}
 });
 
@@ -74,7 +73,10 @@ var GMap = {
 		this.map = new google.maps.Map(document.getElementById(this.containerID),
 		this.defaultOptions);
 
-		this.centerToDefault()
+		this.centerToDefault();
+
+		google.maps.event.trigger(map, 'resize');
+		this.map.setZoom( this.map.getZoom() );
 	},
 	initGeo: function() {
 		var self = this;
