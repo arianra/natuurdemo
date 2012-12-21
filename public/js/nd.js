0e4fc6cc131b1a2995a01524624704ff7bb79793
BA4 <arian@brandambassadors.nl>
2012-12-21 16:52:02 +0100

    map javascript

--- public/js/nd.js
+++ public/js/nd.js
@@ -36,15 +36,19 @@ var remainingHeightPercentage = function (arr, num) {
 $(document).bind('pagechange' , function(){
 	if ($.mobile.activePage.attr('id') == 'pageActiviteit')
 	{
-		console.log( $('#'+GMap.containerID).length )
 		var contentHeight = Math.round(remainingHeightPercentage( [ $('.header-balk-activiteit') , $('.footer-balk-activiteit') , $('.titelblok-activiteit') ] ));
 		$(".main").css({'height':contentHeight + 'px'});
 		$("#map_canvas").css({'height':contentHeight + 'px'});
+
+
 		GMap.init();
+
+
+		$( '.header-knop-zoek' ).on( 'click' , function(){ GMap.initGeo(); } )
 	}
 });
 
-//$( 'header-knop-zoek' ).on( 'click' , function(){ this.preventDefault(); GMap.initGeo(); } )
+
 
 var GMap = {
 	containerID: 'map_canvas',
@@ -70,12 +74,49 @@ var GMap = {
 		this.map = new google.maps.Map(document.getElementById(this.containerID),
 		this.defaultOptions);
 
-		this.map.setCenter( this.defaultLocation );
+		this.centerToDefault()
 	},
 	initGeo: function() {
+		var self = this;
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(function (position) {
+				
+				self.geoLocation = new google.maps.LatLng(position.coords.latitude, 
+				position.coords.longitude);
 
-		point = new google.maps.LatLng(52.354425, 4.896241); 
-		this.map.setCenter( point );
+				self.createMarker( '#marker-geo-location' , self.markerTypes[0] , this.geoLocation , '<p>Ik aanvaard geen halve maatpakken</p>' )
+				self.centerToPoint( this.geoLocation );
+				
+			}, function () {
+				self.handleNoGeolocation(true);
+			});
+		} else {
+			// Browser doesn't support Geolocation
+			self.handleNoGeolocation(false);
+		}
+	},
+	handleNoGeo: function(error) {
+		if(error){
+			//'Error: De geolocotie-service is mislukt.';
+			this.centerToDefault();
+		}
+		else{
+			//'Error: Je browser ondersteund geen geolocation.';
+		}
+	},
+	centerToDefault: function(){
+		this.map.setCenter( this.defaultLocation );
+	},
+	centerToPoint: function( point ){
+		if( arguments.length < 1 )return;
+
+		try{
+			var point = ( point instanceof google.maps.LatLng ) ? point : new new google.maps.LatLng( point[0] , point[1] );
+			this.map.setCenter( point )
+		}
+		catch(err){
+			console.log( err );
+		}
 
 	},
 	createMarker: function( selector , type , point , html ) {
@@ -89,7 +130,7 @@ var GMap = {
 		marker = {
 			selector: selector,
 			content: html,
-			point: new google.maps.LatLng(point[0], point[1]),
+			point: ( point instanceof google.maps.LatLng ) ? point : new google.maps.LatLng(point[0], point[1]),
 			type: type,
 			marker: new google.maps.Marker({
 			position: point,
