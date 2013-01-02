d3c7ffd5070e04adf2acea62f6f2f41199d17763
Tim <jantimon@hotmail.com>
2013-01-02 14:54:03 +0100

    map

--- public/js/nd.js
+++ public/js/nd.js
@@ -34,22 +34,28 @@ var remainingHeightPercentage = function (arr, num) {
 }
 
 $(document).bind('pagechange' , function(){
-	if ($.mobile.activePage.attr('id') == 'pageActiviteit')
+	if ($.mobile.activePage.attr('id') == 'page-activiteit')
 	{
 		var contentHeight = Math.round(remainingHeightPercentage( [ $('.header-balk-activiteit') , $('.footer-balk-activiteit') , $('.titelblok-activiteit') ] ));
 		$(".main").css({'height':contentHeight + 'px'});
 		$("#map_canvas").css({'height':contentHeight + 'px'});
 
-		//GMap.init('map_canvas');
-		$(".header-knop-zoek").click(GMap.init('map_canvas'));
+		GMap.init('map_canvas');
+		//$(".header-knop-zoek").click(GMap.init('map_canvas'));
+	}else if ($.mobile.activePage.attr('id') == 'page-locatie')
+	{
+		var contentHeight = Math.round(remainingHeightPercentage( [ $('.header-balk-locatie') , $('.footer-balk-locatie') , $('.titelblok-locatie') ] ));
+		$(".main").css({'height':contentHeight + 'px'});
+		$("#map-canvas-locatie").css({'height':contentHeight + 'px'});
 
-		//$( '.header-knop-zoek' ).on( 'click' , function(){ GMap.init(); } )
-
+		GMap.init('map-canvas-locatie');
+		//$("#locatie-bevestig-knop").click(GMap.init('map-canvas-locatie'));
 	}
 });
 
-
-
+/*
+*	GOOGLE MAPS FUNCTIONS
+*/
 var GMap = {
 	containerID: 'map_canvas',
 	defaultLocation: new google.maps.LatLng(52.2167, 5.1333),
@@ -80,6 +86,10 @@ var GMap = {
 
 		google.maps.event.trigger(this.map, 'resize');
 		this.map.setZoom( this.map.getZoom() );
+		
+		point = new google.maps.LatLng(52.2167, 5.1333);	
+		marker = this.createMarker('#current','current',point,'<div id="markerTip"><a href="#page-detail" data-transition="slide" data-role="button" data-inline="true" data-corners="true" data-shadow="true" data-iconshadow="true" data-wrapperels="span" data-theme="c"  class="ui-btn ui-shadow ui-btn-corner-all ui-btn-inline ui-btn-hover-c ui-btn-up-c"><span class="ui-btn-inner"><span class="ui-btn-text">Tooltip</span></span></a></div>')
+	
 	},
 	clear: function(){
 		$('#' + this.containerID).find("*").remove();
@@ -141,14 +151,55 @@ var GMap = {
 			point: ( point instanceof google.maps.LatLng ) ? point : new google.maps.LatLng(point[0], point[1]),
 			type: type,
 			marker: new google.maps.Marker({
-			position: point,
-			map: self.map,
-			icon: type['icon'],
-			mouseDownListener: mdListener
+				position: point,
+				map: self.map,
+				clickable: true
+				//icon: type['icon'],
+				//mouseDownListener: mdListener
 			})
 		};
-
 		self.markers.push( marker );
+		
+		// Initialize marker mouse down listener && map mouse down listener
+		self.markerMouseDown(type, html);
+		self.markerDrag(type, html);
+		self.mapMouseDown(type, html);
+	},
+	markerMouseDown: function (type, html){
+		self = this;
+		markerClick: google.maps.event.addListener(marker.marker, 'mousedown', function() { 
+			latLng = this.getPosition(); // returns LatLng object
+			this.map.panTo(latLng); // setCenter takes a LatLng object
+			
+			var contentString = html;
+		
+			// If it already exists - remove it before adding it
+			self.removeMarker();
+			
+			// Only add a tooltip if the clicked marker is not the current location
+			if(type != 'currentz'){
+				$('#mapContainer').append(contentString);// Add tooltip
+				$("#markerTip").css({'position':'absolute','left':Math.round($('#mapContainer').width()/2 - 50) + 'px','top':Math.round($('#mapContainer').height()/2 - 100) + 'px','z-index':'1001' });
+			}
+				
+		});
+	},
+	markerDrag: function (type, html){ // Only works well on touch devices
+		drag: google.maps.event.addListener(marker.marker, "mouseout", function() {
+			// If a markerTip exists - remove it before adding it
+			//self.removeMarker();
+		});
+	},
+	mapMouseDown: function (){
+		mapClick: google.maps.event.addListener(this.map, 'mousedown', function(event) {
+			// If a markerTip exists - remove it before adding it
+			self.removeMarker();
+		});
+	},
+	removeMarker: function(){
+		if ($("#markerTip").length > 0){
+				$('#markerTip').remove(); 
+		}
 	}
 
 }
